8185345575dbf4f6ee135a932aea93bc8456f746
Krinker, Elman <Elman.Krinker@ofr.treasury.gov>
2017-02-27 13:16:02 -0500

    added bootstrap, error handling, and templates with favicon

--- financial_instability.py
+++ financial_instability.py
@@ -1,83 +1,38 @@
-from flask import Flask
-
-import argparse
-from tornado.options import options
-from tornado.wsgi import WSGIContainer
-import signal
-from tornado.ioloop import IOLoop, PeriodicCallback
-from tornado.web import FallbackHandler, Application
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask.ext.script import Manager
+from flask.ext.moment import Moment
+from datetime import datetime
 
-'''
-# --- Tornado specific < ---
-is_closing = False
-# --- Tornado specific > ---
-'''
-'''
-# Process arguments to make sure that port was specified, if not exit
-arg_parser = argparse.ArgumentParser(description='Financial Instability Application')
-arg_parser.add_argument("-p", "--port", help="Port to run application on",
-                        type=int, action="store", dest="tcp_port")
-
-options = arg_parser.parse_args()
-
-if options.tcp_port is None:
-    print "You must specify a port to host on, e.g. python financial_instability.py -p 5000"
-    exit()
-'''
-'''
-# --- Tornado specific < ---
-#  Define Tornado and its set up configuration files
-def signal_handler(signum, frame):
-    global is_closing
-    print('exiting...')
-    is_closing = True
-
-
-def try_exit():
-    global is_closing
-    if is_closing:
-        # clean up here
-        IOLoop.instance().stop()
-        print('exit success')
-# --- Tornado specific > ---
-'''
 financial_instability_app = Flask(__name__)
 manager = Manager(financial_instability_app)
-'''
-# --- Tornado specific < ---
-financial_instability_container = WSGIContainer(financial_instability_app)
-signal.signal(signal.SIGINT, signal_handler)
-PeriodicCallback(try_exit, 100).start()
-
-application = Application([
-    (r".*", FallbackHandler, dict(fallback=financial_instability_container)),
-    ])
-# --- Tornado specific > ---
-'''
+bootstrap = Bootstrap(financial_instability_app)
+moment = Moment(financial_instability_app)
 
 
 # Define routes
 @financial_instability_app.route('/')
 def index():
-    return '<h1>Welcome to Financial Instability</h1>'
+    return render_template("index.html", current_time=datetime.utcnow())
 
 
 @financial_instability_app.route('/user/<name>')
 def user(name):
-    return '<h1>Welcome to Financial Instability, %s</h1>' % name
+    return render_template("user.html", name=name)
+
+
+@financial_instability_app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@financial_instability_app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 print "Available Routes:"
 print list(financial_instability_app.url_map.iter_rules())
 
 # Start the application
 if __name__ == '__main__':
-    '''
-    # --- Tornado specific < ---
-    application.listen(options.tcp_port)
-    IOLoop.instance().start()
-    # --- Tornado specific > ---
-    '''
-    # --- Flask built in server
-    # financial_instability_app.run(debug=True)
     manager.run()
