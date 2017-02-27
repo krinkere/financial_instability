a01f58308d200eed526ce475993bed9cc25bc8f2
Krinker, Elman <Elman.Krinker@ofr.treasury.gov>
2017-02-27 13:57:01 -0500

    added Post/ Redirect/Get pattern

--- financial_instability.py
+++ financial_instability.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
 from datetime import datetime
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 financial_instability_app = Flask(__name__)
 manager = Manager(financial_instability_app)
 bootstrap = Bootstrap(financial_instability_app)
 moment = Moment(financial_instability_app)
 
+# Cross-Site Request Forgery (CSRF) Protection
+financial_instability_app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 # Define routes
-@financial_instability_app.route('/')
+@financial_instability_app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    form = TickerForm()
+    if form.validate_on_submit():
+        session["ticker"] = form.ticker.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, ticker=session.get("ticker"), current_time=datetime.utcnow())
 
 
 @financial_instability_app.route('/user/<name>')
@@ -32,6 +43,11 @@ def internal_server_error(e):
 
 print "Available Routes:"
 print list(financial_instability_app.url_map.iter_rules())
+
+
+class TickerForm(Form):
+    ticker = StringField('Stock symbol?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 # Start the application
 if __name__ == '__main__':
