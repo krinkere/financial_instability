4f645aacc37ff925ea1bf4500196f9c141e47b13
Al Krinker <al.krinker@gmail.com>
2017-06-14 16:39:35 -0400

    bug fix

--- manage.py
+++ manage.py
@@ -42,4 +42,5 @@ def destroy_db():
 if __name__ == '__main__':
     print "Available Routes:"
     print list(financial_instability_app.url_map.iter_rules())
-    manager.run()
+    # manager.run()
+    financial_instability_app.run()
