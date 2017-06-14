7b9bac76a20b349e09f8ae7d256087a2b2f42382
Al Krinker <al.krinker@gmail.com>
2017-06-14 16:56:45 -0400

    bug fix

--- financial_instability_app/__init__.py
+++ financial_instability_app/__init__.py
@@ -14,9 +14,9 @@ def create_app(config_name):
     financial_instability_app.config.from_object(config[config_name])
     config[config_name].init_app(financial_instability_app)
 
-    #bootstrap.init_app(financial_instability_app)
-    #moment.init_app(financial_instability_app)
-    #db.init_app(financial_instability_app)
+    bootstrap.init_app(financial_instability_app)
+    moment.init_app(financial_instability_app)
+    db.init_app(financial_instability_app)
 
     from .main import main as main_blueprint
     financial_instability_app.register_blueprint(main_blueprint)
